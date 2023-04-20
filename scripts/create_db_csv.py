be168abbaf104395487438c4fbac89f853704da8
dral-code <draltahn@gmail.com>
2023-04-20 19:12:40 +0200

    Add script to generate db csv

---
+++ scripts/create_db_csv.py
@@ -0,0 +1,15 @@
+import os
+
+filename = "files/list_of_dns_entry.csv"
+if os.path.exists(filename):
+    os.remove(filename)
+
+file = open (filename, "a")
+file.write("dnsEntryName;dnsEntryIP\n")
+
+for domain in range(0,250):
+    for host in range(0,250):
+        #print ("host" + str(host) + ".domain" + str(domain) + ".rsx218-dox.cnam.fr;10.0." + str(domain) + "." + str(host))
+        file.write ("host" + str(host) + ".domain" + str(domain) + ".rsx218-dox.cnam.fr;10.0." + str(domain) + "." + str(host) +"\n")
+
+file.close()            
