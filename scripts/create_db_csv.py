779e78dc90eebaf90c3671acb004618fd5c46247
dral-code <draltahn@gmail.com>
2023-04-20 21:08:02 +0200

    Change network to avoid colision with virtualbox NAT address

--- scripts/create_db_csv.py
+++ scripts/create_db_csv.py
@@ -9,7 +9,7 @@ file.write("dnsEntryName;dnsEntryIP\n")
 
 for domain in range(0,251):
     for host in range(1,251):
-        #print ("host" + str(host) + ".domain" + str(domain) + ".rsx218-dox.cnam.fr;10.0." + str(domain) + "." + str(host))
-        file.write ("host" + str(host) + ".domain" + str(domain) + ".rsx218-dox.cnam.fr;10.0." + str(domain) + "." + str(host) +"\n")
+        #print ("host" + str(host) + ".domain" + str(domain) + ".rsx218-dox.cnam.fr;172.16." + str(domain) + "." + str(host))
+        file.write ("host" + str(host) + ".domain" + str(domain) + ".rsx218-dox.cnam.fr;172.16." + str(domain) + "." + str(host) +"\n")
 
 file.close()            
--- scripts/create_db_yml.py
+++ scripts/create_db_yml.py
@@ -5,12 +5,12 @@ if os.path.exists(filename):
     os.remove(filename)
 
 file = open (filename, "a")
-file.write("---\n# vars file for Bind setup\ndomain: rsx218-dox.cnam.fr\n\nrev_domain: 0.10.in-addr.arpa\n\nrecords:\n")
+file.write("---\n# vars file for Bind setup\ndomain: rsx218-dox.cnam.fr\n\nrev_domain: 16.172.in-addr.arpa\n\nrecords:\n")
 
 for domain in range(0,251):
     for host in range(1,251):
-        #print ("    host" + str(host) + ".domain" + str(domain) + ": {forward: 10.0." + str(domain) + "." + str(host) + ", type: A, last: " + str(host) + "." + str(domain) + ", rev: 0.10.in-addr.arpa.}\n")
-        file.write ("    host" + str(host) + ".domain" + str(domain) + ": {forward: 10.0." + str(domain) + "." + str(host) + ", type: A, last: " + str(host) + "." + str(domain) + ", rev: 0.10.in-addr.arpa.}\n")
+        #print ("    host" + str(host) + ".domain" + str(domain) + ": {forward: 172.16." + str(domain) + "." + str(host) + ", type: A, last: " + str(host) + "." + str(domain) + ", rev: 16.172.in-addr.arpa.}\n")
+        file.write ("    host" + str(host) + ".domain" + str(domain) + ": {forward: 172.16." + str(domain) + "." + str(host) + ", type: A, last: " + str(host) + "." + str(domain) + ", rev: 16.172.in-addr.arpa.}\n")
 
 file.close()
 print ("Copy files/main.yml to the path ansible/roles/dns-bind9/vars/.")
