32480d1cbb3896e3fa2d0b8435dbe940cf0190c6
dral-code <draltahn@gmail.com>
2023-04-20 20:37:08 +0200

    Update yaml script

--- scripts/create_db_yml.py
+++ scripts/create_db_yml.py
@@ -1,15 +1,22 @@
 import os
 
-filename = "files/vars.yml"
+filename = "files/main.yml"
 if os.path.exists(filename):
     os.remove(filename)
 
 file = open (filename, "a")
-file.write("records:\n")
+file.write("---\
+# vars file for Bind setup\
+domain: rsx218-dox.cnam.fr\
+\
+rev_domain: 0.10.in-addr.arpa\
+\
+records:\n")
 
 for domain in range(0,251):
     for host in range(1,251):
         #print ("    host" + str(host) + ".domain" + str(domain) + ": {forward: 10.0." + str(domain) + "." + str(host) + ", type: A, last: " + str(host) + "." + str(domain) + ", rev: 0.10.in-addr.arpa.}\n")
         file.write ("    host" + str(host) + ".domain" + str(domain) + ": {forward: 10.0." + str(domain) + "." + str(host) + ", type: A, last: " + str(host) + "." + str(domain) + ", rev: 0.10.in-addr.arpa.}\n")
 
-file.close()            
+file.close()
+print ("Copy files/main.yml to the path ansible/roles/dns-bind9/vars/.")
