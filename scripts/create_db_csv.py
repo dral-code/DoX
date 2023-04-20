055053075a96b50a32c896a78a58d5ceed4486e9
dral-code <draltahn@gmail.com>
2023-04-20 20:31:14 +0200

    Correction of host ip cannot be a network address

--- scripts/create_db_csv.py
+++ scripts/create_db_csv.py
@@ -7,8 +7,8 @@ if os.path.exists(filename):
 file = open (filename, "a")
 file.write("dnsEntryName;dnsEntryIP\n")
 
-for domain in range(0,250):
-    for host in range(0,250):
+for domain in range(0,251):
+    for host in range(1,251):
         #print ("host" + str(host) + ".domain" + str(domain) + ".rsx218-dox.cnam.fr;10.0." + str(domain) + "." + str(host))
         file.write ("host" + str(host) + ".domain" + str(domain) + ".rsx218-dox.cnam.fr;10.0." + str(domain) + "." + str(host) +"\n")
 
