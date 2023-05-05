f17fa8ee4164a25cf92a44b1bafc37be92c0505f
dral-code <draltahn@gmail.com>
2023-05-05 23:21:31 +0200

    optimze python client script

--- scripts/request_client_dns.py
+++ scripts/request_client_dns.py
@@ -1,19 +1,21 @@
 import subprocess
-import pandas 
+import random
 
-#csvfile = pandas.read_csv('../shared_files/list_of_dns_entry.csv', sep=';')
-csvfile = pandas.read_csv('list_of_dns_entry.csv', sep=';')
-csvfile = csvfile.sample(frac=1).reset_index(drop=True)
-#print( csvfile)
+n = 250
+ihost = [i for i in range (1, n+1)]
+random.shuffle(ihost)
+idomain = [i for i in range (0, n+1)]
+random.shuffle(idomain)
 
 line_count = 0
-for index, line in csvfile.iterrows():
-    dev_null = " &> /dev/null"
-    #dev_null = ""
-    cmd_str = "dnslookup " + line["dnsEntryName"] + " 192.168.56.2:453" + dev_null
-    # ip: " + line['dnsEntryIP'])
-    subprocess.run(cmd_str, shell=True)
-    line_count += 1
-    print("request num : " + str(line_count) + " for " + line["dnsEntryName"])
-#    if line_count == 5:
-#        break
+for d in idomain:
+    for h in ihost:
+        dev_null = " > /dev/null 2>&1"
+        #dev_null = ""
+        url = "host" + str(h) + ".domain" + str(d) + ".rsx218-dox.cnam.fr"
+        cmd_str = "nohup dnslookup host" + url + " 192.168.56.2:453" + dev_null + " &"
+        subprocess.run(cmd_str, shell=True)
+        line_count += 1
+        print("request num : " + str(line_count) + " for " + url)
+#        if line_count == 5:
+#            break
